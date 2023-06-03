c265d7611a91e3a67af5979e8c5cfaf897e13a12
apricity-site <treeshadeliu@outlook.com>
2023-06-04 00:41:46 +0800

    feat: button scss

--- play/src/Button.tsx
+++ play/src/Button.tsx
@@ -1,13 +1,15 @@
-import {
-  Button,
-  ConfigProvider,
-} from '@ant-design-solid/component'
+import { Button, ConfigProvider } from '@ant-design-solid/component'
 
 export default () => {
   return (
     <ConfigProvider>
-      <Button>默认按钮</Button>
-      <Button type="primary">主要按钮</Button>
+      <div style={{ display: 'inline-flex', gap: '8px' }}>
+        <Button type="default">default按钮</Button>
+        <Button type="primary">primary按钮</Button>
+        <Button type="dashed">dashed按钮</Button>
+        <Button type="text">text按钮</Button>
+        <Button type="link">link按钮</Button>
+      </div>
     </ConfigProvider>
   )
 }

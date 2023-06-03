da596f4ae4b7286cfa1d192dbb6bb89e3cf0cf96
apricity-site <treeshadeliu@outlook.com>
2023-06-03 19:52:39 +0800

    feat: css var

--- packages/ant-design-solid/index.ts
+++ packages/ant-design-solid/index.ts
@@ -1,1 +1,13 @@
+import { GlobalConfig, setGlobaConfigEntirely } from '@ant-design-solid/hooks'
+
 export * from '@ant-design-solid/component'
+
+const intializeTheme = () => {}
+
+export default {
+  // TODO theme initialize
+  setup(config?: GlobalConfig) {
+    setGlobaConfigEntirely(config)
+    intializeTheme()
+  }
+}

b15bdfc6c447a6d261dd08a41bedc9fe782696e8
apricity-site <treeshadeliu@outlook.com>
2023-06-07 22:53:54 +0800

    feat: disabled

--- packages/component/Button/Button.tsx
+++ packages/component/Button/Button.tsx
@@ -70,7 +70,9 @@ export const Button: Component<ButtonProps> = (props) => {
         !!withDefaultProps.children && !!withDefaultProps.icon
       ),
       ns.is('danger', !!withDefaultProps.danger),
-      ns.is('icon-only', !withDefaultProps.children && !!withDefaultProps.icon)
+      ns.is('icon-only', !withDefaultProps.children && !!withDefaultProps.icon),
+      ns.is('block', withDefaultProps.block),
+      ns.is('ghost', withDefaultProps.ghost)
     ])
   const clickHandler = (event: MouseEvent) => {
     if (!withDefaultProps.disabled) {
@@ -96,13 +98,31 @@ export const Button: Component<ButtonProps> = (props) => {
   const icon = withDefaultProps.icon ? (
     <span class={ns.e('icon')}> {withDefaultProps.icon} </span>
   ) : null
+  const maybeLoading = withDefaultProps.loading ? (
+    <span class={classnames(ns.e('icon'), ns.em('icon', 'loading'))}>
+      <span class={classnames(ns.e('spin'))}>
+        <svg
+          viewBox="0 0 1024 1024"
+          data-icon="loading"
+          width="1em"
+          height="1em"
+          fill="currentColor"
+          aria-hidden="true"
+        >
+          <path d="M988 548c-19.9 0-36-16.1-36-36 0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 00-94.3-139.9 437.71 437.71 0 00-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7 26.7 63.1 40.2 130.2 40.2 199.3.1 19.9-16 36-35.9 36z"></path>
+        </svg>
+      </span>
+    </span>
+  ) : (
+    icon
+  )
   return (
     <button
       type={withDefaultProps.htmlType}
       class={clz()}
       onClick={clickHandler}
     >
-      {icon}
+      {maybeLoading}
       <span>{children(() => props.children)()}</span>
     </button>
   )
--- play/src/Button.tsx
+++ play/src/Button.tsx
@@ -7,255 +7,289 @@ export default () => {
   const [size, setSize] = createSignal<ComponentSize>('middle')
   return (
     <ConfigProvider>
-      <h1>Type</h1>
-      <div style={{ display: 'inline-flex', gap: '8px' }}>
-        <Button type="default">Default Button</Button>
-        <Button type="primary">primary Button</Button>
-        <Button type="dashed">dashed Button</Button>
-        <Button type="text">text Button</Button>
-        <Button type="link" href="www.baidu.com">
-          link Button
-        </Button>
-        <Button type="link">link Button</Button>
-      </div>
-      <h1>Icon</h1>
-      <div style={{ display: 'inline-flex', gap: '8px' }}>
-        <Button
-          type="primary"
-          shape="circle"
-          icon={
-            <Icon>
-              <SearchOutlined />
-            </Icon>
-          }
-        />
-        <Button type="primary" shape="circle">
-          A
-        </Button>
-        <Button
-          type="primary"
-          icon={
-            <Icon>
-              <SearchOutlined />
-            </Icon>
-          }
+      <div style={{ height: '100%', width: '100%', padding: '20px 20px' }}>
+        <h1>Type</h1>
+        <div style={{ display: 'inline-flex', gap: '8px' }}>
+          <Button type="default">Default Button</Button>
+          <Button type="primary">primary Button</Button>
+          <Button type="dashed">dashed Button</Button>
+          <Button type="text">text Button</Button>
+          <Button type="link" href="www.baidu.com">
+            link Button
+          </Button>
+          <Button type="link">link Button</Button>
+        </div>
+        <h1>Icon</h1>
+        <div style={{ display: 'inline-flex', gap: '8px' }}>
+          <Button
+            type="primary"
+            shape="circle"
+            icon={
+              <Icon>
+                <SearchOutlined />
+              </Icon>
+            }
+          />
+          <Button type="primary" shape="circle">
+            A
+          </Button>
+          <Button
+            type="primary"
+            icon={
+              <Icon>
+                <SearchOutlined />
+              </Icon>
+            }
+          >
+            搜索
+          </Button>
+          <Button
+            shape="circle"
+            icon={
+              <Icon>
+                <SearchOutlined />
+              </Icon>
+            }
+          />
+          <Button shape="circle">A</Button>
+          <Button
+            icon={
+              <Icon>
+                <SearchOutlined />
+              </Icon>
+            }
+          >
+            搜索
+          </Button>
+          <Button
+            type="dashed"
+            shape="circle"
+            icon={
+              <Icon>
+                <SearchOutlined />
+              </Icon>
+            }
+          />
+          <Button type="dashed" shape="circle">
+            A
+          </Button>
+          <Button
+            type="dashed"
+            icon={
+              <Icon>
+                <SearchOutlined />
+              </Icon>
+            }
+          >
+            搜索
+          </Button>
+        </div>
+        <h1>Size</h1>
+        <Button onClick={() => setSize('small')}>small</Button>
+        <Button onClick={() => setSize('middle')}>middle</Button>
+        <Button onClick={() => setSize('large')}>large</Button>
+        <div style={{ display: 'flex', gap: '8px', 'margin-top': '8px' }}>
+          <Button
+            size={size()}
+            type="primary"
+            shape="circle"
+            icon={
+              <Icon>
+                <SearchOutlined />
+              </Icon>
+            }
+          />
+          <Button size={size()} type="primary" shape="circle">
+            A
+          </Button>
+          <Button
+            size={size()}
+            type="primary"
+            icon={
+              <Icon>
+                <SearchOutlined />
+              </Icon>
+            }
+          >
+            搜索
+          </Button>
+          <Button
+            size={size()}
+            shape="circle"
+            icon={
+              <Icon>
+                <SearchOutlined />
+              </Icon>
+            }
+          />
+          <Button size={size()} shape="circle">
+            A
+          </Button>
+          <Button
+            size={size()}
+            icon={
+              <Icon>
+                <SearchOutlined />
+              </Icon>
+            }
+          >
+            搜索
+          </Button>
+          <Button
+            size={size()}
+            type="dashed"
+            shape="circle"
+            icon={
+              <Icon>
+                <SearchOutlined />
+              </Icon>
+            }
+          />
+          <Button size={size()} type="dashed" shape="circle">
+            A
+          </Button>
+          <Button
+            size={size()}
+            type="dashed"
+            icon={
+              <Icon>
+                <SearchOutlined />
+              </Icon>
+            }
+          >
+            搜索
+          </Button>
+        </div>
+        <h1>Shape</h1>
+        <div style={{ display: 'flex', gap: '8px' }}>
+          <Button
+            shape="circle"
+            icon={
+              <Icon>
+                <SearchOutlined />
+              </Icon>
+            }
+          />
+          <Button
+            shape="round"
+            icon={
+              <Icon>
+                <SearchOutlined />
+              </Icon>
+            }
+          >
+            搜索
+          </Button>
+          <Button
+            shape="circle"
+            type="primary"
+            icon={
+              <Icon>
+                <SearchOutlined />
+              </Icon>
+            }
+          />
+          <Button
+            shape="round"
+            type="primary"
+            icon={
+              <Icon>
+                <SearchOutlined />
+              </Icon>
+            }
+          >
+            搜索
+          </Button>
+        </div>
+        <h1>Disabled</h1>
+        <div
+          style={{ display: 'flex', 'flex-direction': 'column', gap: '8px' }}
         >
-          搜索
-        </Button>
-        <Button
-          shape="circle"
-          icon={
-            <Icon>
-              <SearchOutlined />
-            </Icon>
-          }
-        />
-        <Button shape="circle">A</Button>
-        <Button
-          icon={
-            <Icon>
-              <SearchOutlined />
-            </Icon>
-          }
-        >
-          搜索
-        </Button>
-        <Button
-          type="dashed"
-          shape="circle"
-          icon={
-            <Icon>
-              <SearchOutlined />
-            </Icon>
-          }
-        />
-        <Button type="dashed" shape="circle">
-          A
-        </Button>
-        <Button
-          type="dashed"
-          icon={
-            <Icon>
-              <SearchOutlined />
-            </Icon>
-          }
-        >
-          搜索
-        </Button>
-      </div>
-      <h1>Size</h1>
-      <Button onClick={() => setSize('small')}>small</Button>
-      <Button onClick={() => setSize('middle')}>middle</Button>
-      <Button onClick={() => setSize('large')}>large</Button>
-      <div style={{ display: 'flex', gap: '8px', 'margin-top': '8px' }}>
-        <Button
-          size={size()}
-          type="primary"
-          shape="circle"
-          icon={
-            <Icon>
-              <SearchOutlined />
-            </Icon>
-          }
-        />
-        <Button size={size()} type="primary" shape="circle">
-          A
-        </Button>
-        <Button
-          size={size()}
-          type="primary"
-          icon={
-            <Icon>
-              <SearchOutlined />
-            </Icon>
-          }
-        >
-          搜索
-        </Button>
-        <Button
-          size={size()}
-          shape="circle"
-          icon={
-            <Icon>
-              <SearchOutlined />
-            </Icon>
-          }
-        />
-        <Button size={size()} shape="circle">
-          A
-        </Button>
-        <Button
-          size={size()}
-          icon={
-            <Icon>
-              <SearchOutlined />
-            </Icon>
-          }
-        >
-          搜索
-        </Button>
-        <Button
-          size={size()}
-          type="dashed"
-          shape="circle"
-          icon={
-            <Icon>
-              <SearchOutlined />
-            </Icon>
-          }
-        />
-        <Button size={size()} type="dashed" shape="circle">
-          A
-        </Button>
-        <Button
-          size={size()}
-          type="dashed"
-          icon={
-            <Icon>
-              <SearchOutlined />
-            </Icon>
-          }
-        >
-          搜索
-        </Button>
-      </div>
-      <h1>Shape</h1>
-      <div style={{ display: 'flex', gap: '8px' }}>
-        <Button
-          shape="circle"
-          icon={
-            <Icon>
-              <SearchOutlined />
-            </Icon>
-          }
-        />
-        <Button
-          shape="round"
-          icon={
-            <Icon>
-              <SearchOutlined />
-            </Icon>
-          }
-        >
-          搜索
-        </Button>
-        <Button
-          shape="circle"
-          type="primary"
-          icon={
-            <Icon>
-              <SearchOutlined />
-            </Icon>
-          }
-        />
-        <Button
-          shape="round"
-          type="primary"
-          icon={
-            <Icon>
-              <SearchOutlined />
-            </Icon>
-          }
-        >
-          搜索
-        </Button>
-      </div>
-      <h1>Disabled</h1>
-      <div style={{ display: 'flex', 'flex-direction': 'column', gap: '8px' }}>
+          <div class="space">
+            <Button type="primary">Primary</Button>
+            <Button
+              type="primary"
+              disabled
+              onClick={() => console.log('click')}
+            >
+              Disabled Primary
+            </Button>
+          </div>
+          <div class="space">
+            <Button>Primary</Button>
+            <Button disabled onClick={() => console.log('click')}>
+              Disabled Default
+            </Button>
+          </div>
+          <div class="space">
+            <Button type="dashed">Dashed</Button>
+            <Button type="dashed" disabled>
+              Disabled Disaled
+            </Button>
+          </div>
+          <div>
+            <Button type="text">Text</Button>
+            <Button type="text" disabled>
+              Text Disaled
+            </Button>
+          </div>
+          <div>
+            <Button type="link">Link</Button>
+            <Button type="link" disabled>
+              Link Disaled
+            </Button>
+          </div>
+          <div class="space">
+            <Button type="primary" href="https://ant.design/index-cn">
+              Href Primary
+            </Button>
+            <Button type="primary" href="https://ant.design/index-cn" disabled>
+              Href Primary(disabled)
+            </Button>
+          </div>
+        </div>
+        <h1>Danger</h1>
         <div class="space">
-          <Button type="primary">Primary</Button>
-          <Button type="primary" disabled onClick={() => console.log('click')}>
-            Disabled Primary
+          <Button type="primary" danger>
+            Primary
+          </Button>
+          <Button danger>Default</Button>
+          <Button type="dashed" danger>
+            Dashed
+          </Button>
+          <Button type="text" danger>
+            Text
+          </Button>
+          <Button type="link" danger>
+            Link
           </Button>
         </div>
+        <h1>GHOST</h1>
         <div class="space">
-          <Button>Primary</Button>
-          <Button disabled onClick={() => console.log('click')}>
-            Disabled Default
+          <Button type="primary" ghost>
+            Primary
+          </Button>
+          <Button ghost>Default</Button>
+          <Button type="dashed" ghost>
+            Dashed
+          </Button>
+          <Button type="primary" danger ghost>
+            Danger
           </Button>
         </div>
+        <h1>Loading</h1>
         <div class="space">
-          <Button type="dashed">Dashed</Button>
-          <Button type="dashed" disabled>
-            Disabled Disaled
+          <Button type="primary" loading>
+            Primary
+          </Button>
+          <Button>Default</Button>
+          <Button type="dashed">
+            Dashed
+          </Button>
+          <Button type="primary">
+            Danger
           </Button>
         </div>
-        <div>
-          <Button type="text">Text</Button>
-          <Button type="text" disabled>
-            Text Disaled
-          </Button>
-        </div>
-        <div>
-          <Button type="link">Link</Button>
-          <Button type="link" disabled>
-            Link Disaled
-          </Button>
-        </div>
-        <div class='space'>
-          <Button type="primary" href="https://ant.design/index-cn">
-            Href Primary
-          </Button>
-          <Button type="primary" href="https://ant.design/index-cn" disabled>
-            Href Primary(disabled)
-          </Button>
-        </div>
-      </div>
-      <h1>Danger</h1>
-      <div class="space">
-        <Button type="primary" danger>
-          Primary
-        </Button>
-        <Button danger>Default</Button>
-        <Button type="dashed" danger>
-          Dashed
-        </Button>
-        <Button type="text" danger>
-          Text
-        </Button>
-        <Button type="link" danger>
-          Link
-        </Button>
       </div>
     </ConfigProvider>
   )
